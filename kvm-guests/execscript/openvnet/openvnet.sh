4adb1d88fbdebea48d93188fccde0bbf20557f75
Masahito Yoshida <masahito@axsh.net>
2014-09-06 16:39:16 +0900

    download repo files from github.

--- kvm-guests/execscript/openvnet/openvnet.sh
+++ kvm-guests/execscript/openvnet/openvnet.sh
@@ -9,5 +9,14 @@ set -o
 chroot_dir=${1}
 
 chroot $1 $SHELL -ex <<'EOS'
+  until curl -fsSkL -o /etc/yum.repos.d/openvnet.repo https://raw.githubusercontent.com/axsh/openvnet/master/openvnet.repo; do
+    sleep 1
+  done
+  until curl -fsSkL -o /etc/yum.repos.d/openvnet-third-party.repo https://raw.githubusercontent.com/axsh/openvnet/master/openvnet-third-party.repo; do
+    sleep 1
+  done
+EOS
+
+chroot $1 $SHELL -ex <<'EOS'
   yum install --disablerepo=updates -y openvnet
 EOS
