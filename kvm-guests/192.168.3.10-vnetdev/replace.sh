9736316cec413c132152426f0f33ecd3f90ee0e0
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-09-25 16:16:14 +0900

    Add new setting for vnet

---
+++ kvm-guests/192.168.3.10-vnetdev/replace.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -x
+
+target_node=192.168.3.10
+ssh_target=jenkins@${target_node}
+
+distro_ver=6.5
+[[ -a distro_ver.conf ]] && . distro_ver.conf
+box_path=../../boxes/kemukins-${distro_ver}-x86_64.kvm.box
+
+function network_connection?() {
+  local ipaddr=${1}
+  ping -c 1 -W 3 ${ipaddr}
+}
+
+if network_connection? ${target_node}; then
+  ssh ${ssh_target} sudo shutdown -h now
+  sleep 20
+  sync
+fi
+
+sudo $SHELL -e <<EOS
+  time tar zxvf ${box_path}
+  time sync
+
+  ./kemukins-init.sh
+  ./run.sh
+EOS
