b5f1ac949603c2cf43e6526f43a77e681e248fa0
Masahito Yoshida <masahito@axsh.net>
2014-10-07 01:53:43 +0900

    replace shutdown with kill.

--- kvm-guests/93-vteskins/run.sh
+++ kvm-guests/93-vteskins/run.sh
@@ -12,6 +12,8 @@ set -o pipefail
 [[ -f ./metadata/vmspec.conf ]]
 .     ./metadata/vmspec.conf
 
+.     ../common/qemu-kvm.sh
+
 #
 vnc_addr=127.0.0.1
 vnc_port=$((11000 + ${offset}))
@@ -37,6 +39,7 @@ function qemu_kvm_path() {
   echo ${command_path}
 }
 
+kill_remove_pidfile ${pidfile}
 $(qemu_kvm_path) -name ${name} \
  -cpu ${cpu_type} \
  -m ${mem_size} \
