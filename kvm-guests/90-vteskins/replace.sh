d7fa98ed4c5a6fec920b35f83f4e37680f2f7f79
Masahito Yoshida <masahito@axsh.net>
2014-10-07 02:09:56 +0900

    remove unnecessary lines.

--- kvm-guests/90-vteskins/replace.sh
+++ kvm-guests/90-vteskins/replace.sh
@@ -6,23 +6,9 @@
 set -e
 set -x
 
-target_node=192.168.2.90
-ssh_target=jenkins@${target_node}
-
 distro_ver=6.4
 [[ -a distro_ver.conf ]] && . distro_ver.conf
 box_path=../../boxes/kemukins-${distro_ver}-x86_64.kvm.box
-
-function network_connection?() {
-  local ipaddr=${1}
-  ping -c 1 -W 3 ${ipaddr}
-}
-
-if network_connection? ${target_node}; then
-  ssh ${ssh_target} sudo shutdown -h now
-  sleep 20
-  sync
-fi
 
 sudo /bin/bash -e <<EOS
   time tar zxvf ${box_path}
