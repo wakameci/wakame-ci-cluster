e93f6c5de34b51f86277d22fef570972413b220e
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-10-09 20:17:38 +0900

    LXC can catch IP address from OpenVNet DHCP

--- kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
+++ kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
@@ -18,6 +18,16 @@ chroot $1 /bin/bash -ex <<EOS
   echo root:root | sudo chroot /var/lib/lxc/lxc1/rootfs chpasswd
 EOS
 
-echo 'lxc.network.hwaddr = 00:18:51:e5:35:01' >> $1/var/lib/lxc/lxc1/config
-echo 'lxc.network.veth.pair = veth_kvm1lxc1'  >> $1/var/lib/lxc/lxc1/config
-sed -i -e 's/^\(lxc.network.link\)/#\1/'         $1/var/lib/lxc/lxc1/config
+cat > $1/var/lib/lxc/lxc1/config <<'EOS'
+lxc.network.type = veth
+lxc.network.flags = up
+#lxc.network.link = virbr0
+lxc.rootfs = /var/lib/lxc/lxc1/rootfs
+lxc.include = /usr/share/lxc/config/centos.common.conf
+lxc.arch = x86_64
+lxc.utsname = lxc1
+lxc.autodev = 0
+lxc.network.name = eth0
+lxc.network.hwaddr = 00:18:51:e5:35:01
+lxc.network.veth.pair = veth_kvm1lxc1
+EOS
