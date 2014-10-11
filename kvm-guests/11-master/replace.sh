73b5bc461949a9954f25be4c6f004d9d9ecb2290
Masahito Yoshida <masahito@axsh.net>
2014-10-11 16:37:48 +0900

    move shutting down to stop.sh

--- kvm-guests/11-master/replace.sh
+++ kvm-guests/11-master/replace.sh
@@ -6,23 +6,13 @@
 set -e
 set -x
 
-target_node=172.16.255.11
-ssh_target=jenkins@${target_node}
-
 distro_ver=6.5
 [[ -a distro_ver.conf ]] && . distro_ver.conf
 box_path=../../boxes/kemukins-${distro_ver}-x86_64.kvm.box
 
-function network_connection?() {
-  local ipaddr=${1}
-  ping -c 1 -W 3 ${ipaddr}
-}
-
-if network_connection? ${target_node}; then
-  ssh ${ssh_target} sudo shutdown -h now
-  sleep 20
-  sync
-fi
+./stop.sh
+sleep 20
+sync
 
 sudo /bin/bash -e <<'EOS'
   timestamp=$(date +%Y%m%d.%s).$$
---
+++ kvm-guests/11-master/stop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+[[ -f ./metadata/vmspec.conf ]]
+.     ./metadata/vmspec.conf
+
+#
+pidfile=kvm.pid
+monitor_addr=127.0.0.1
+monitor_port=$((14000 + ${offset}))
+
+function shutdown_remove_pidfile() {
+  local pidfile=${1} monitor_addr=${2} monitor_port=${3}
+  [[ -f ${pidfile} ]] || return 0
+  local pid=`head -1 ${pidfile}`
+  if ps -p ${pid}; then
+    echo system_powerdown | nc ${monitor_addr} ${monitor_port}
+  fi
+  rm -f ${pidfile}
+  sleep 3
+}
+
+shutdown_remove_pidfile ${pidfile} ${monitor_addr} ${monitor_port}
