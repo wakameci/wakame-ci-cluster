3aed914352176389a0adaff29169b87eca414007
Masahito Yoshida <masahito@axsh.net>
2014-10-11 17:20:29 +0900

    fix to use sudo.

--- kvm-guests/11-master/replace.sh
+++ kvm-guests/11-master/replace.sh
@@ -10,7 +10,7 @@ distro_ver=6.5
 [[ -a distro_ver.conf ]] && . distro_ver.conf
 box_path=../../boxes/kemukins-${distro_ver}-x86_64.kvm.box
 
-./stop.sh
+sudo ./stop.sh
 
 sudo /bin/bash -e <<'EOS'
   timestamp=$(date +%Y%m%d.%s).$$
