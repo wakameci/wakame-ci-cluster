3389341be18c2daa53c9597decd61f8c2e53b37e
Masahito Yoshida <masahito@axsh.net>
2014-10-11 22:45:25 +0900

    use bash syntax.

--- kvm-guests/11-master/stop.sh
+++ kvm-guests/11-master/stop.sh
@@ -19,7 +19,7 @@ function shutdown_remove_pidfile() {
   local pidfile=${1} monitor_addr=${2} monitor_port=${3}
   [[ -f ${pidfile} ]] || return 0
 
-  local pid=`head -1 ${pidfile}`
+  local pid=$(head -1 ${pidfile})
   if ps -p ${pid}; then
     echo system_powerdown | nc ${monitor_addr} ${monitor_port}
 
