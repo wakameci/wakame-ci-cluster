33dd62d407c85bcd334f4d885fe709acd58858c2
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-10-16 21:47:34 +0900

    Hotfix adding sleep before touch webapi

--- kvm-guests/192.168.3.10-vnetdev/vnetscript/0000-runall.sh
+++ kvm-guests/192.168.3.10-vnetdev/vnetscript/0000-runall.sh
@@ -8,6 +8,12 @@ set -o pipefail
 
 01-init-database.sh
 02-start-vnmgr-webapi.sh
+
+### xxx HOTFIX: Some sleep is needed for wait webapi open.
+### xxx Detail: https://github.com/axsh/openvnet/issues/269
+sleep 3
+### xxx
+
 03-setup-openvnet.sh
 04-start-vna.sh
 05-start-lxc.sh
