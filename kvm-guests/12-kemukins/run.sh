3e97fd61a9af0771c6d159d65d6841849251ab8c
Masahito Yoshida <masahito@axsh.net>
2014-10-19 00:33:34 +0900

    organize run.sh.

--- kvm-guests/12-kemukins/run.sh
+++ kvm-guests/12-kemukins/run.sh
@@ -12,6 +12,8 @@ set -o pipefail
 [[ -f ./metadata/vmspec.conf ]]
 .     ./metadata/vmspec.conf
 
+.     ../common/qemu-kvm.sh
+
 #
 vnc_addr=127.0.0.1
 vnc_port=$((11000 + ${offset}))
@@ -37,6 +39,7 @@ function qemu_kvm_path() {
   echo ${command_path}
 }
 
+kill_remove_pidfile ${pidfile}
 $(qemu_kvm_path) -name ${name} \
  -cpu ${cpu_type} \
  -m ${mem_size} \
@@ -47,11 +50,21 @@ $(qemu_kvm_path) -name ${name} \
  -monitor telnet:127.0.0.1:${monitor_port},server,nowait \
  -serial telnet:${serial_addr}:${serial_port},server,nowait \
  -drive file=./box-disk1-head.qcow2,media=disk,boot=on,index=0,cache=none,if=virtio \
- $([[ -f ./box-disk2.raw ]] && echo -drive file=./box-disk2.raw,media=disk,boot=off,index=1,cache=none,if=virtio ) \
- -netdev tap,ifname=${name}-${monitor_port},id=hostnet0,script=,downscript= \
- -device ${nic_driver},netdev=hostnet0,mac=52:54:00:$(date +%H:%M:%S),bus=pci.0,addr=0x3 \
+ $([[ -f ./box-disk2.raw ]] && echo -drive file=./box-disk2.raw,media=disk,boot=off,index=1,cache=none,if=virtio) \
+ $(
+ i=0
+ for brname in ${brnames[@]}; do
+   echo -netdev tap,ifname=${name}-${monitor_port}-${i},id=hostnet${i},script=,downscript=
+   echo -device ${nic_driver},netdev=hostnet${i},mac=${macs[${i}]},bus=pci.0,addr=0x$((3 + ${i}))
+   i=$((${i} + 1))
+ done
+ ) \
  -pidfile ${pidfile} \
  -daemonize
 
-ip link set ${name}-${monitor_port} up
-brctl addif ${brname} ${name}-${monitor_port}
+i=0
+for brname in ${brnames[@]}; do
+  ip link set ${name}-${monitor_port}-${i} up
+  brctl addif ${brnames[${i}]} ${name}-${monitor_port}-${i}
+  i=$((${i} + 1))
+done
