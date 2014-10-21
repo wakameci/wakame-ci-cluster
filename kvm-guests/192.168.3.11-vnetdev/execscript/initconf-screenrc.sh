a8020814e9f7d36e1d87d217fd5bfedd241d1f20
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-10-21 17:45:56 +0900

    Add .screenrc for KVM1 and KVM2

---
+++ kvm-guests/192.168.3.11-vnetdev/execscript/initconf-screenrc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+
+chroot_dir=${1}
+ipaddr=10.0.1.10
+
+cat > $1/home/kemukins/.screenrc <<'EOS'
+escape ^Ta
+caption always '%?%F%{= mW}%:%{= Kk}%?%2n%f%07=%t%='
+hardstatus alwayslastline '%m/%d %02c %{= .m}%H%{-} %L=%-w%45L>%{=u m.}%n %t%{-}%+w %-17<%=%{= .y}(%l)'
+EOS
