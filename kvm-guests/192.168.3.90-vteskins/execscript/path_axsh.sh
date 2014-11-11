9cb2a5e6607a8aca30045a5fd2797cf84640aaaf
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-11-11 19:31:10 +0900

    Path to axsh apps

---
+++ kvm-guests/192.168.3.90-vteskins/execscript/path_axsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+
+chroot_dir=${1}
+
+chroot $1 /bin/bash -ex <<'EOS'
+  echo 'export PATH=$PATH:/opt/axsh/openvnet/ruby/bin:/opt/axsh/openvnet/vnctl/bin:/opt/axsh/vnetscript' >> ~root/.bash_profile
+  echo 'export PATH=$PATH:/opt/axsh/openvnet/ruby/bin:/opt/axsh/openvnet/vnctl/bin:/opt/axsh/vnetscript' >> ~kemukins/.bash_profile
+EOS
