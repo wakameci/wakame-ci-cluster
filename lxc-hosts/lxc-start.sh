f7ec26f9828be9f605a7ec7e9c023600d10a34f6
Masahito Yoshida <masahito@axsh.net>
2014-11-28 11:56:33 +0900

    add lxc-start.sh.

---
+++ lxc-hosts/lxc-start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+# usage:
+#  $0 <ctid>
+#
+set -e
+set -o pipefail
+set -x
+
+ctid=${1}
+if [[ -z "${ctid}" ]]; then
+  echo "$0 <ctid>" >&2
+  exit 1
+fi
+
+lxc-start -n ${ctid} -d -l DEBUG -o /var/log/lxc/${ctid}.log
