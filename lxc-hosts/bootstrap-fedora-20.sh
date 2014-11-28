bc192055002a9c09b7d4cd8aa32ea2b2a2af6b59
Masahito Yoshida <masahito@axsh.net>
2014-11-28 19:23:28 +0900

    use lxc-start.sh.

--- lxc-hosts/bootstrap-fedora-20.sh
+++ lxc-hosts/bootstrap-fedora-20.sh
@@ -169,23 +169,7 @@ umount ${rootfs_path}/proc
 
 ### start container
 
-lxc-start -n ${ctid} -d -l DEBUG -o /var/log/lxc/${ctid}.log
-
-### add device
-
-lxc-device -n ${ctid} add /dev/kvm
-lxc-device -n ${ctid} add /dev/net/tun
-
-# > PTY allocation request failed on channel 0
-lxc-device -n ${ctid} add /dev/ptmx
-
-# /dev/loopX and /dev/dm-X
-for i in {0..127}; do
-lxc-attach -n ${ctid} -- bash -ex <<-EOS
-  [[ -b /dev/loop${i} ]] || mknod /dev/loop${i} -m 660 b   7 ${i}
-  [[ -b /dev/dm-${i}  ]] || mknod /dev/dm-${i}  -m 660 b 253 ${i}
-EOS
-done
+./lxc-start.sh ${ctid}
 
 # setup kvm-host
 lxc-attach -n ${ctid} -- bash -ex <<EOS
