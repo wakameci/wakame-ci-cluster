7e5e8a6c99d6f4e3d9c63a262e3bddc66fae5d65
Masahito Yoshida <masahito@axsh.net>
2014-12-19 19:55:31 +0900

    add qemu_command to qemu-kvm.sh.

--- kvm-guests/common/resume.sh
+++ kvm-guests/common/resume.sh
@@ -18,32 +18,7 @@ set -o pipefail
 kill_remove_pidfile ${pidfile}
 
 #
-$(qemu_kvm_path) -name ${name} \
- -cpu ${cpu_type} \
- -m ${mem_size} \
- -smp ${cpu_num} \
- -vnc ${vnc_addr}:${vnc_port} \
- -k en-us \
- -rtc ${rtc} \
- -monitor telnet:127.0.0.1:${monitor_port},server,nowait \
- -serial telnet:${serial_addr}:${serial_port},server,nowait \
- -serial ${console} \
- -drive file=./box-disk1-head.qcow2,media=disk,boot=on,index=0,cache=none,if=${drive_type} \
- $([[ -f ./box-disk2.raw ]] && echo -drive file=./box-disk2.raw,media=disk,boot=off,index=1,cache=none,if=${drive_type}) \
- $(
- i=0
- for brname in ${brnames[@]}; do
-   echo -netdev tap,ifname=${name}-${monitor_port}-${i},id=hostnet${i},script=,downscript=
-   echo -device ${nic_driver},netdev=hostnet${i},mac=${macs[${i}]},bus=pci.0,addr=0x$((3 + ${i}))
-   i=$((${i} + 1))
- done
- ) \
- -chardev socket,port=${qmp_port},host=${qmp_addr},server,nowait,id=qga0 \
- -device virtio-serial \
- -device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0 \
- -pidfile ${pidfile} \
- -incoming "exec: cat $(pwd)/kvm.state" \
- -daemonize
+eval $(qemu_command) -incoming \"exec: cat $(pwd)/kvm.state\"
 
 #
 i=0
