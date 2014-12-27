e1bae0e542557f5ab270a8e4679d745994682cb6
Masahito Yoshida <masahito@axsh.net>
2014-12-28 01:28:36 +0900

    replace openvnet.sh with buildbook.

--- kvm-guests/execscript/openvnet/openvnet.sh
+++ kvm-guests/execscript/openvnet/openvnet.sh
@@ -21,14 +21,5 @@ chroot $1 /bin/bash -ex <<'EOS'
 EOS
 
 chroot $1 /bin/bash -ex <<'EOS'
-  until curl -fsSkL -o /etc/yum.repos.d/openvnet.repo https://raw.githubusercontent.com/axsh/openvnet/master/openvnet.repo; do
-    sleep 1
-  done
-  until curl -fsSkL -o /etc/yum.repos.d/openvnet-third-party.repo https://raw.githubusercontent.com/axsh/openvnet/master/openvnet-third-party.repo; do
-    sleep 1
-  done
-EOS
-
-chroot $1 /bin/bash -ex <<'EOS'
   yum install --disablerepo=updates -y openvnet
 EOS
