f5b80506f8b1b77cfd154a697377eead5fbee5dd
Masahito Yoshida <masahito@axsh.net>
2014-12-28 13:06:46 +0900

    refactor replace.sh/run.sh.

--- kvm-guests/192.168.3.10-vnetdev/replace.sh
+++ kvm-guests/192.168.3.10-vnetdev/replace.sh
@@ -7,23 +7,9 @@ set -e
 set -o pipefail
 set -x
 
-target_node=192.168.3.10
-ssh_target=jenkins@${target_node}
-
 distro_ver=6.5
 [[ -a distro_ver.conf ]] && . distro_ver.conf
 box_path=../../boxes/kemumaki-${distro_ver}-x86_64.kvm.box
-
-function network_connection?() {
-  local ipaddr=${1}
-  ping -c 1 -W 3 ${ipaddr}
-}
-
-if network_connection? ${target_node}; then
-  ssh ${ssh_target} sudo shutdown -h now
-  sleep 20
-  sync
-fi
 
 sudo /bin/bash -e <<EOS
   ../common/stop.sh
@@ -31,5 +17,6 @@ sudo /bin/bash -e <<EOS
   ../common/unpack-box.sh ${box_path}
 
   ../common/kemumaki-init.sh
+  ../common/qcow2-init.sh
   ./run.sh
 EOS
