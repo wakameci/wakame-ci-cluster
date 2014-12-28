ea7c44381915d621e66a22f10e7100ab7ffbe7bd
Masahito Yoshida <masahito@axsh.net>
2014-12-28 13:02:33 +0900

    replace lxc installation with buildbook.

--- kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
+++ kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
@@ -9,10 +9,6 @@ set -o pipefail
 chroot_dir=${1}
 centos_ver=6.5
 
-chroot $1 /bin/bash -ex <<'EOS'
-  yum install --disablerepo=updates -y lxc lxc-libs lxc-templates
-EOS
-
 chroot $1 /bin/bash -ex <<EOS
   lxc-create -t centos -n lxc1 -- -R ${centos_ver}
   echo root:root | sudo chroot /var/lib/lxc/lxc1/rootfs chpasswd
