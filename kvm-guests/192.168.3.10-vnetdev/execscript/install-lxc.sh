c816a7adb75dd64527e3bc989c7718cd36102a4e
Kenny Debrauwer <kenny@axsh.net>
2015-03-10 19:27:40 +0900

    refactoring LXC configuration part 1

--- kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
+++ kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
@@ -9,33 +9,30 @@ set -o pipefail
 chroot_dir=${1}
 centos_ver=6.6
 lxc_count=2
+mac_start=1
 
 . ../common/lxc-init.sh ${chroot_dir} ${centos_ver} ${lxc_count}
 
-cat > $1/var/lib/lxc/lxc1/config <<'EOS'
-lxc.network.type = veth
-lxc.network.flags = up
-#lxc.network.link = virbr0
-lxc.rootfs = /var/lib/lxc/lxc1/rootfs
-lxc.include = /usr/share/lxc/config/centos.common.conf
-lxc.arch = x86_64
-lxc.utsname = lxc1
-lxc.autodev = 0
-lxc.network.name = eth0
-lxc.network.hwaddr = 00:18:51:e5:35:01
-lxc.network.veth.pair = veth_kvm1lxc1
-EOS
+COUNTER=0
+hex=00
+while [  $COUNTER -lt ${lxc_count} ]; do
+  let COUNTER=COUNTER+1  
+  let mac_hex=$(printf '%x\n' $mac_start)
 
-cat > $1/var/lib/lxc/lxc2/config <<'EOS'
-lxc.network.type = veth
-lxc.network.flags = up
-#lxc.network.link = virbr0
-lxc.rootfs = /var/lib/lxc/lxc2/rootfs
-lxc.include = /usr/share/lxc/config/centos.common.conf
-lxc.arch = x86_64
-lxc.utsname = lxc2
-lxc.autodev = 0
-lxc.network.name = eth0
-lxc.network.hwaddr = 00:18:51:e5:35:02
-lxc.network.veth.pair = veth_kvm1lxc2
+  cat > ${chroot_dir}/var/lib/lxc/lxc${COUNTER}/config <<EOS
+    lxc.network.type = veth
+    lxc.network.flags = up
+    #lxc.network.link = virbr0
+    lxc.rootfs = /var/lib/lxc/lxc${COUNTER}/rootfs
+    lxc.include = /usr/share/lxc/config/centos.common.conf
+    lxc.arch = x86_64
+    lxc.utsname = lxc${COUNTER}
+    lxc.autodev = 0
+    lxc.network.name = eth0
+    lxc.network.hwaddr = 00:18:51:e5:35:${mac_hex}
+    lxc.network.veth.pair = veth_kvm1lxc${COUNTER}
+
 EOS
+  let mac_start=mac_start+1
+done
+
