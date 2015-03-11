562ed9eb895e2d77374cdd742c0a26d915fd8838
Kenny Debrauwer <kenny@axsh.net>
2015-03-11 12:45:47 +0900

    refactoring LXC configuration part 2

--- kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
+++ kvm-guests/192.168.3.10-vnetdev/execscript/install-lxc.sh
@@ -8,31 +8,10 @@ set -o pipefail
 
 chroot_dir=${1}
 centos_ver=6.6
+kvm_suffix=1
 lxc_count=2
 mac_start=1
 
-. ../common/lxc-init.sh ${chroot_dir} ${centos_ver} ${lxc_count}
+. ../common/lxc-init.sh ${chroot_dir} ${centos_ver} ${kvm_suffix} ${lxc_count} ${mac_start}
 
-COUNTER=0
-hex=00
-while [  $COUNTER -lt ${lxc_count} ]; do
-  let COUNTER=COUNTER+1  
-  let mac_hex=$(printf '%x\n' $mac_start)
-
-  cat > ${chroot_dir}/var/lib/lxc/lxc${COUNTER}/config <<EOS
-    lxc.network.type = veth
-    lxc.network.flags = up
-    #lxc.network.link = virbr0
-    lxc.rootfs = /var/lib/lxc/lxc${COUNTER}/rootfs
-    lxc.include = /usr/share/lxc/config/centos.common.conf
-    lxc.arch = x86_64
-    lxc.utsname = lxc${COUNTER}
-    lxc.autodev = 0
-    lxc.network.name = eth0
-    lxc.network.hwaddr = 00:18:51:e5:35:${mac_hex}
-    lxc.network.veth.pair = veth_kvm1lxc${COUNTER}
-
-EOS
-  let mac_start=mac_start+1
-done
 
--- kvm-guests/common/lxc-init.sh
+++ kvm-guests/common/lxc-init.sh
@@ -8,7 +8,9 @@ set -o pipefail
 
 chroot_dir=${1}
 centos_ver=${2}
-lxc_count=${3}
+kvm_suffix=${3}
+lxc_count=${4}
+mac_start=${5}
 
 COUNTER=0
 while [  $COUNTER -lt ${lxc_count} ]; do
@@ -17,6 +19,28 @@ while [  $COUNTER -lt ${lxc_count} ]; do
     lxc-create -t centos -n lxc${COUNTER} -- -R ${centos_ver}
     echo root:root | sudo chroot /var/lib/lxc/lxc${COUNTER}/rootfs chpasswd
 EOS
+done
+
+COUNTER=0
+while [  $COUNTER -lt ${lxc_count} ]; do
+  let COUNTER=COUNTER+1  
+  let mac_hex=$(printf '%02x\n' $mac_start)
+
+  cat > ${chroot_dir}/var/lib/lxc/lxc${COUNTER}/config <<EOS
+    lxc.network.type = veth
+    lxc.network.flags = up
+    #lxc.network.link = virbr0
+    lxc.rootfs = /var/lib/lxc/lxc${COUNTER}/rootfs
+    lxc.include = /usr/share/lxc/config/centos.common.conf
+    lxc.arch = x86_64
+    lxc.utsname = lxc${COUNTER}
+    lxc.autodev = 0
+    lxc.network.name = eth0
+    lxc.network.hwaddr = 00:18:51:e5:35:${mac_hex}
+    lxc.network.veth.pair = veth_kvm${kvm_suffix}lxc${COUNTER}
+
+EOS
+  let mac_start=mac_start+1
 done
 
 . ../common/lxc/iperf3.sh ${chroot_dir} ${centos_ver} ${lxc_count}
