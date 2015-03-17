2b1e63fa929af4f7b168fe088c412bf198be7f16
Masahito Yoshida <masahito@axsh.net>
2015-03-17 23:03:15 +0900

    add kagechiyo box.

--- boxes/download-boxes.sh
+++ boxes/download-boxes.sh
@@ -16,10 +16,12 @@ boxes="
      minimal-6.4-x86_64.kvm.box
      minimal-6.5-x86_64.kvm.box
      minimal-6.6-x86_64.kvm.box
+   kagechiyo-6.6-x86_64.kvm.box
 
     kemumaki-7.0.1406-x86_64.kvm.box
  lxckemumaki-7.0.1406-x86_64.kvm.box
      minimal-7.0.1406-x86_64.kvm.box
+   kagechiyo-7.0.1406-x86_64.kvm.box
 "
 
 function download_file() {
