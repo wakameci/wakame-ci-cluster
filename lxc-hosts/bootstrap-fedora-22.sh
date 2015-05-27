58038a4f9d729d1446501dec7666c6109a8b19ab
Masahito Yoshida <masahito@axsh.net>
2015-05-28 01:14:59 +0900

    reenable setup-fedora-22.

--- lxc-hosts/bootstrap-fedora-22.sh
+++ lxc-hosts/bootstrap-fedora-22.sh
@@ -189,17 +189,17 @@ umount ${rootfs_path}/proc
 "${BASH_SOURCE[0]%/*}/lxc-start.sh" "${ctid}"
 
 # setup kvm-host
-#lxc-attach -n ${ctid} -- bash -ex <<EOS
-#  cd /tmp
-#  until curl -fsSkL https://raw.githubusercontent.com/wakameci/wakame-ci-cluster/master/kvm-hosts/setup-${distro_name}-${distro_ver}.sh -o ./setup-${distro_name}-${distro_ver}.sh; do
-#    sleep 1
-#  done
-#  chmod +x ./setup-${distro_name}-${distro_ver}.sh
-#  sed -i s,--disablerepo=updates,, ./setup-${distro_name}-${distro_ver}.sh
-#  ls -l ./setup-${distro_name}-${distro_ver}.sh
-#  ./setup-${distro_name}-${distro_ver}.sh
-#  rm ./setup-${distro_name}-${distro_ver}.sh
-#EOS
+lxc-attach -n ${ctid} -- bash -ex <<EOS
+  cd /tmp
+  until curl -fsSkL https://raw.githubusercontent.com/wakameci/wakame-ci-cluster/master/kvm-hosts/setup-${distro_name}-${distro_ver}.sh -o ./setup-${distro_name}-${distro_ver}.sh; do
+    sleep 1
+  done
+  chmod +x ./setup-${distro_name}-${distro_ver}.sh
+  sed -i s,--disablerepo=updates,, ./setup-${distro_name}-${distro_ver}.sh
+  ls -l ./setup-${distro_name}-${distro_ver}.sh
+  ./setup-${distro_name}-${distro_ver}.sh
+  rm ./setup-${distro_name}-${distro_ver}.sh
+EOS
 
 # warm up device-mapper
 "${BASH_SOURCE[0]%/*}/lxc-dmwarmup.sh" "${ctid}"
