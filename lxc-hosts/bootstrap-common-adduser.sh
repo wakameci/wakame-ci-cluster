04198f7817294895de13a70de24186792ac650a7
Masahito Yoshida <masahito@axsh.net>
2015-07-01 21:35:09 +0900

    update adduser list.

--- lxc-hosts/bootstrap-common-adduser.sh
+++ lxc-hosts/bootstrap-common-adduser.sh
@@ -13,4 +13,12 @@ chroot ${rootfs_path} bash -ex <<EOS
   /usr/local/bin/add-github-user.sh axsh
   /usr/local/bin/add-github-user.sh hansode
   /usr/local/bin/add-github-user.sh t-iwano
+  /usr/local/bin/add-github-user.sh unakatsuo
+  /usr/local/bin/add-github-user.sh metallion
+  /usr/local/bin/add-github-user.sh rakshasa
+  /usr/local/bin/add-github-user.sh k-oyakata
+  /usr/local/bin/add-github-user.sh akry
+  /usr/local/bin/add-github-user.sh triggers
+  /usr/local/bin/add-github-user.sh toros11
+  /usr/local/bin/add-github-user.sh mopster
 EOS
