23656cb29d028c5d7b39d5d295a4aa4ab27d632f
Masahito Yoshida <masahito@axsh.net>
2015-08-26 18:26:14 +0900

    install redis.

--- kvm-guests/91-vteskins/execscript/service-mysqld.sh
+++ kvm-guests/91-vteskins/execscript/service-mysqld.sh
@@ -8,10 +8,6 @@ set -o pipefail
 
 chroot_dir=${1}
 
-chroot $1 $SHELL -ex <<'EOS'
-  rpm -qi mysql-server >/dev/null || { yum install --disablerepo=updates -y mysql-server; }
-EOS
-
 chroot $1 /bin/bash -ex <<'EOS'
   chkconfig mysqld on
 EOS
